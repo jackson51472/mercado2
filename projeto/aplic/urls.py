b2dcdd04184af08432bc7c15521a85a90779046d
Jackson <jacksonaparesido@gmail.com>
2023-11-13 18:24:10 -0300

    login e cadastro

--- projeto/aplic/urls.py
+++ projeto/aplic/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, MyView, DetalhesProdutoView
+from .views import IndexView, MyView, DetalhesProdutoView, CadastroLogin
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("mine/", MyView.as_view(), name="my-view"),
     path("detalhes/<int:id>",DetalhesProdutoView.as_view(), name="detalhe_produto"),
+    path("login/", CadastroLogin.login, name="login"),
+    path("cadastro/", CadastroLogin.cadastro, name="cadastro")
 ]
--- projeto/aplic/views.py
+++ projeto/aplic/views.py
@@ -1,10 +1,47 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 from typing import Any
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate
+from aplic.models import Produto, Cliente
 
-from aplic.models import Produto
+
+class CadastroLogin():
+    def cadastro (request):
+        if request.method == "GET":
+            return render(request, "cadastro.html")
+        else:
+            nome = request.POST.get("nome")
+            cpf = request.POST.get("cpf")
+
+            user = Cliente.objects.filter(cpf=cpf).first()
+
+            if user:
+                return HttpResponse("Já existe usuario com esse cpf")
+            
+        
+            user = Cliente.objects.create(nome=nome, cpf=cpf)
+            user.save()
+
+            return HttpResponse("Cadastrado")
+
+    def login (request):
+        if request.method == "GET":
+            return render(request, "login.html")
+        else:
+            nome = request.POST.get("nome")
+            cpf = request.POST.get("cpf")
+
+            user = authenticate(nome=nome, cpf=cpf)
+
+            if user:
+                return HttpResponse("Logado")
+            else:
+                return HttpResponse("Email ou senha invalido")
+
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -25,9 +62,6 @@ class DetalhesProdutoView(ListView):
         id = self.kwargs['id']
         context['produto'] = Produto.objects.filter(id=id).first
         return context
-
-    
-
 
 class MyView(View):
     def get(self, request, *args, **kwargs):
